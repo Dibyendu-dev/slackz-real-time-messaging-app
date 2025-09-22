5374e3258c43b1747ad633f531b2cc5674bcfcf9
ddas4548 <ddas4548@gmail.com>
2025-09-22 21:50:43 +0530

    db  setup

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,11 @@
-import express from 'express'
+import express from 'express';
 import { StatusCodes } from 'http-status-codes';
 import { PORT } from './config/serverConfig.js';
-
+import connectDB from './config/dbConfig.js';
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/ping', (req, res) => {
   return res.status(StatusCodes.OK).json({ message: 'pong' });
@@ -10,4 +13,9 @@ app.get('/ping', (req, res) => {
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
+  connectDB()
 });
+
+// ddas4548_db_user
+// 97wl2RKDZW4ElD4M
+// mongodb+srv://ddas4548_db_user:97wl2RKDZW4ElD4M@cluster0.82wqjsf.mongodb.net/
