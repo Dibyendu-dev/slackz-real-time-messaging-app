2ac56d661e94df5cc211d331e70a2710b0236e6a
ddas4548 <ddas4548@gmail.com>
2025-09-23 09:13:35 +0530

    added besic router layer

--- backend/index.js
+++ backend/index.js
@@ -2,10 +2,15 @@ import express from 'express';
 import { StatusCodes } from 'http-status-codes';
 import { PORT } from './config/serverConfig.js';
 import connectDB from './config/dbConfig.js';
+import apiRouter from './routes/apiRoute.js'
+
+
 const app = express();
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use('/api',apiRouter)
 
 app.get('/ping', (req, res) => {
   return res.status(StatusCodes.OK).json({ message: 'pong' });
